5bd0d5f43e77e7398fef947fa675cfb76a467772
HaiLeVN <hailtse172908@fpt.edu.vn>
2024-05-12 14:46:51 +0700

    feat: register and login jwt token features and role based authorization

---
+++ BE-SheritonHotel/Core/OtherObjects/StaticUserRoles.cs
@@ -0,0 +1,10 @@
+﻿namespace JwtAuthAspNet7WebAPI.Core.OtherObjects
+{
+    public static class StaticUserRoles
+    {
+        public const string STAFF = "STAFF";
+        public const string ADMIN = "ADMIN";
+        public const string CUSTOMER = "CUSTOMER";
+
+    }
+}
