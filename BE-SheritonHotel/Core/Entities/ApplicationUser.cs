068e2e2cb2c311bcaafc3c6f6875350edee35789
Thanh138 <thanhncse172947@fpt.edu.vn>
2024-05-31 21:18:51 +0700

    Revert "Merge branch 'develop' of https://github.com/SWD392-CustomizableHotel/BE into S2-code-first-db"
    This reverts commit ebc7a9f994c6469c84d8180396cbbcf21f461391, reversing
    changes made to 5651b5defcb2ad52caa23fd67c32db98ec0fe5b6.

--- BE-SheritonHotel/Core/Entities/ApplicationUser.cs
+++ BE-SheritonHotel/Core/Entities/ApplicationUser.cs
@@ -1,4 +1,4 @@
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Identity;
 
@@ -10,11 +10,6 @@ namespace Core.Entities
         public string LastName { get; set; }
         public bool IsDeleted { get; set; }
         public DateTime CreatedDate { get; set; }
-
-        [ForeignKey("RoleId")]
-        public int RoleId { get; set; }
-
-        public virtual Role Role { get; set; } = null!;
 
         public virtual ICollection<Booking> Bookings { get; set; } = new List<Booking>();
     }
