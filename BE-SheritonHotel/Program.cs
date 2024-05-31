6a3da093c1b8dca548b8d8c4944cac0ecd0658d3
kieutranquocson5 <kieutranquocson5@gmail.com>
2024-05-31 23:47:12 +0700

    chore: migration and update connectionString

--- BE-SheritonHotel/Program.cs
+++ BE-SheritonHotel/Program.cs
@@ -20,7 +20,7 @@ builder.Services.AddSwaggerGen();
 // Add DB
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
-    var connectionString = builder.Configuration.GetConnectionString("local");
+    var connectionString = builder.Configuration.GetConnectionString("serverDatabase");
     options.UseSqlServer(connectionString);
 });
 
@@ -31,7 +31,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(MyAllowSpecificOrigins,
                           policy =>
                           {
-                              policy.WithOrigins("http://localhost:4200")
+                              policy.WithOrigins("http://localhost:4200", "https://fe-customizablehotel.vercel.app/")
                                                   .AllowAnyHeader()
                                                   .AllowAnyMethod();
                           });
