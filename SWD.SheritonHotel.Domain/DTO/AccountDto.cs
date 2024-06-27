c0489e127337509c42606b23930e75cfa2d83773
minhuy2411 <hoanganh241115@gmail.com>
2024-06-27 21:27:08 +0700

    feat: account management

--- SWD.SheritonHotel.Domain/DTO/AccountDto.cs
+++ SWD.SheritonHotel.Domain/DTO/AccountDto.cs
@@ -12,6 +12,7 @@ public class AccountDto
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public DateTime? Dob { get; set; }
-    public bool IsActive { get; set; }
+
+    public bool IsActive { get; set; } = true;
     public List<string> Roles { get; set; }
 }
